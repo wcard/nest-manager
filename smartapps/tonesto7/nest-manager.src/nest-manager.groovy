5e1c1c6ef400c2a6fea85c125cca9e87e0c70d1e
Winston Card <winstonedwardcard@gmail.com>
2019-11-03 16:54:52 -0500

    change another oauth

--- smartapps/tonesto7/nest-manager.src/nest-manager.groovy
+++ smartapps/tonesto7/nest-manager.src/nest-manager.groovy
@@ -7408,7 +7408,8 @@ def callback() {
 				client_secret: clientSecret(),
 				grant_type: "authorization_code",
 			]
-			def tokenUrl = "https://api.home.nest.com/oauth2/access_token?${toQueryString(tokenParams)}"
+			def tokenUrl = "https://accounts.google.com/signin/oauth/oauthchooseaccount?client_id=733249279899-44tchle2kaa9afr5v9ov7jbuojfr9lrq.apps.googleusercontent.com&as=Rmz8rv3n3v5Yv1k2FMDGIA&destination=https%3A%2F%2Fhome.nest.com&approval_state=!ChREVjNHX20wMjZabEpoTUppM3hQThIfczRocDNxTFBSTjhTd0ktM2lWMU9TaWRYR2VjejR4WQ%E2%88%99AJDr988AAAAAXcCc4J4Psc2dFyFVnWCRtV6ze7_HZRAE&oauthgdpr=1&oauthriskyscope=1&xsrfsig=ChkAeAh8T7iiY9NvNRPv9pIhZH2A29RhsYTWEg5hcHByb3ZhbF9zdGF0ZRILZGVzdGluYXRpb24SBXNvYWN1Eg9vYXV0aHJpc2t5c2NvcGU&flowName=GeneralOAuthFlow"
+            //"https://api.home.nest.com/oauth2/access_token?${toQueryString(tokenParams)}"
 			httpPost(uri: tokenUrl) { resp ->
 				atomicState.authToken = resp?.data.access_token
 				if(atomicState?.authToken) {
